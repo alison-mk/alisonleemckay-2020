36e86add20939e3390a78921e6fb3d9e6b19d7fa
Alison McKay <mckay.ali@gmail.com>
2020-01-06 23:34:19 -0800

    update footer content

--- footer.php
+++ footer.php
@@ -2,14 +2,13 @@
 	<h3>Contact</h3>
 	<ul>
 		<li class="instagram"><a href="http://instagram.com/alison_mk" target="_blank" title="Instagram">Instagram</a></li>
-		<li><a href="" title="Github">Github</a></li>
+		<li><a href="https://github.com/amckayay" target="_blank" title="Github">Github</a></li>
 		<li class="linkedin"><a href="https://www.linkedin.com/in/alison-mckay-6b724035/" target="_blank" title="LinkedIn">LinkedIn</a></li>
 		<li class="facebook"><a href="http://www.facebook.com/alisonleemckay" target="_blank" title="Facebook">Facebook</a></li>		
 	</ul>
 	<ul class="email">
 		<li><a href="mailto:alisonleemckay@gmail.com" target="_blank" title="Email me">alisonleemckay@gmail.com</a></li>
 	</ul>
-
 	<p><em><small>This site is set in Abhaya Libre, a typeface designed by Sol Matas and Pushpanada Ekanayake.</small></em></p>
 </footer>
 
