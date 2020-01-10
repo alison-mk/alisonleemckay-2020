1317e4c5f2fcf0d215faaeab47c0274e54ee204c
Alison McKay <mckay.ali@gmail.com>
2020-01-09 20:32:51 -0800

    remove unused jquery from repo and footer

--- footer.php
+++ footer.php
@@ -12,7 +12,6 @@
 	<p><em><small>This site is set in Abhaya Libre, a typeface designed by Sol Matas and Pushpanada Ekanayake.</small></em></p>
 </footer>
 
-<!-- <script src="js/jquery-3.3.1.min.js"></script> -->
 <script src="js/scripts.js"></script>
 </div>
 </body>
